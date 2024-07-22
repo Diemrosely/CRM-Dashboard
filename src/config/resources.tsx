8f7d1282faa519cdc1a18ce642481bb5a437fd86
Diem Ly <diemrosely@gmail.com>
2024-07-22 16:44:11 -0500

    added upcoming-events component, applied sorting and pagination, also pulled in db of example data from refine

---
+++ src/config/resources.tsx
@@ -0,0 +1,44 @@
+import { DashOutlined, ProjectOutlined, ShopOutlined } from "@ant-design/icons";
+import { IResourceItem } from "@refinedev/core";
+
+export const resources: IResourceItem[] = [
+    /** 
+    * A resource in Refine performs these actions: 
+    * list -> get all records (Read)
+    * show -> get a single record (Read)
+    * create -> create a record (Create)
+    * edit -> update a record (Update)
+    * delete -> delete a record (Delete)
+    * or clone
+    */
+   {
+    name: 'dashboard',
+    list: '/',
+    meta: {
+        label: 'Dashboard',
+        icon: <DashOutlined />
+        }
+    },
+    {
+        name: 'companies',
+        list: '/companies',
+        show: '/companies/:id',
+        create: '/companies/new',
+        edit: '/companies/edit/:id',
+        meta: {
+            label: 'Companies',
+            icon: <ShopOutlined />
+        }
+    },
+    {
+        name: 'tasks',
+        list: '/tasks',
+        show: '/tasks/:id',
+        create: '/tasks/new',
+        edit: '/tasks/edit/:id',
+        meta: {
+            label: 'Tasks',
+            icon: <ProjectOutlined />
+        }
+    }
+]
